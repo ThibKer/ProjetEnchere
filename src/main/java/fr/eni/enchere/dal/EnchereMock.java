0b9763b96def48d924dd8465979ba4b0d241a303
Lilian CHIROL <lilian.chirol2021@campus-eni.fr>
2021-07-06 12:00:42 +0200

    DAO mock(no Update)

---
+++ src/main/java/fr/eni/enchere/dal/EnchereMock.java
@@ -0,0 +1,33 @@
+package fr.eni.enchere.dal;
+
+import java.util.List;
+
+import fr.eni.enchere.bo.Enchere;
+
+public class EnchereMock implements EnchereDAO {
+
+	@Override
+	public void insert(Enchere enchere) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Enchere> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Enchere enchere) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Enchere enchere) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
