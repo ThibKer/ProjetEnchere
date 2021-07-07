d9862c0494b8eaf2dd47e160a12eb670f0c69c9a
Thibaut Kerouedan <thibaut.kerouedan2021@campus-eni.fr>
2021-07-07 11:50:34 +0200

    FIX retour du fichier UtilisateurManagerImpl

---
+++ src/main/java/fr/eni/enchere/bll/UtilisateurManagerImpl.java
@@ -0,0 +1,20 @@
+package fr.eni.enchere.bll;
+
+import java.util.List;
+
+import fr.eni.enchere.bo.Utilisateur;
+import fr.eni.enchere.dal.FactoryDAO;
+import fr.eni.enchere.dal.Utilisateur.UtilisateurDAO;
+
+public class UtilisateurManagerImpl implements UtilisateurManager {
+	private static UtilisateurDAO dao =  FactoryDAO.getUtilisateurDAO();
+	@Override
+	public void addUtilisateur(Utilisateur utilisateur) {
+		dao.insert(utilisateur);
+	}
+
+	@Override
+	public List<Utilisateur> getAllUtilisateurs() {
+		return dao.getAll();
+	}
+}
