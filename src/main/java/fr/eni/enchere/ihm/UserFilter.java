7d1b798f603bb8199d81a03ec26f36a0b66a247b
Thibaut Kerouedan <thibaut.kerouedan2021@campus-eni.fr>
2021-07-08 12:46:54 +0200

    OK now

--- src/main/java/fr/eni/enchere/ihm/UserFilter.java
+++ src/main/java/fr/eni/enchere/ihm/UserFilter.java
@@ -1,10 +1,7 @@
 package fr.eni.enchere.ihm;
 
 import java.io.IOException;
-<<<<<<< HEAD
 
-=======
->>>>>>> 7305f828b0cabac32fdd0c2cefd22f5e928b9361
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -12,11 +9,10 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-<<<<<<< HEAD
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-=======
->>>>>>> 7305f828b0cabac32fdd0c2cefd22f5e928b9361
+
 
 /**
  * Servlet Filter implementation class UserFilter
@@ -42,7 +38,6 @@ public class UserFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		// TODO Auto-generated method stub
 		// place your code here
-<<<<<<< HEAD
 		Object loggin = request.getServletContext().getAttribute("User");
 		if(loggin != null) {
 			System.out.println("quelqu'un est logger");
@@ -69,12 +64,7 @@ public class UserFilter implements Filter {
 	      
 	      // call next filter in the chain : let j_security_check authenticate 
 	      // user
-	      
-=======
-		System.out.println("ca filtre !!");
-		// pass the request along the filter chain
-		chain.doFilter(request, response);
->>>>>>> 7305f828b0cabac32fdd0c2cefd22f5e928b9361
+	     
 	}
 
 	/**
